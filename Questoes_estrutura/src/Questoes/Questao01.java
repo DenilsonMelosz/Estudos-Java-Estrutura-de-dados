7edef6a29cf7a00e7695c0474a5b770edd05399b
Denilson Melo <113272495+DenilsonMelosz@users.noreply.github.com>
2024-04-03 21:14:31 -0300

    Arquivos da Primeira Questão
    Questão 1 da lista

---
+++ Questoes_estrutura/src/Questoes/Questao01.java
@@ -0,0 +1,26 @@
+package Questoes;
+
+import java.util.Stack;
+
+public class Questao01 {
+
+	public static void main(String[] args) {
+        Stack<Integer> pilha = new Stack<>();
+        	pilha.push(1);
+	        pilha.push(2);
+	        pilha.push(3);
+	        pilha.push(4);
+
+	        // Acima estamos mostrando na tela as pilhas
+	        System.out.println("Pilha antes das operacoes: " + pilha);
+		
+	       pilha.pop();
+	       pilha.pop();
+	       pilha.pop();
+	       pilha.push(3);
+	       pilha.push(4);
+	       pilha.push(2);
+	       System.out.println("Pilha apos as operacoes: " + pilha);
+	}
+
+}
---
+++ Questoes_estrutura/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 200028969
+ *
+ */
+module Questoes_estrutura {
+}
